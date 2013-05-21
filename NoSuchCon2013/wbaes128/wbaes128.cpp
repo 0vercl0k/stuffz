c8167b29f44d1d1db8381f66fdd05e9ace3fb687
Axel "0vercl0k" Souchet <0vercl0k@tuxfamily.org>
2013-05-21 20:24:33 +0200

    typoops.

--- NoSuchCon2013/wbaes128/wbaes128.cpp
+++ NoSuchCon2013/wbaes128/wbaes128.cpp
@@ -15,7 +15,7 @@
     You should have received a copy of the GNU General Public License
     along with this program.  If not, see <http://www.gnu.org/licenses/>.
     
-    Kudos for @elvanderb for that really awesome-tough-motherfuckin-challenge.
+    Kudos to @elvanderb for that really awesome-tough-motherfuckin-challenge.
     @kutioo source of entropy since 1337.
 */
 #include <stdio.h>
