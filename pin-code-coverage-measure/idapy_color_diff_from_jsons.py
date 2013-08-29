dae62e842c18298e3103149c518db8d8417e5203
Axel "0vercl0k" Souchet <0vercl0k@tuxfamily.org>
2013-08-29 23:43:48 +0200

    use directly the defaultdict constructor

--- pin-code-coverage-measure/idapy_color_diff_from_jsons.py
+++ pin-code-coverage-measure/idapy_color_diff_from_jsons.py
@@ -26,13 +26,13 @@ from collections import defaultdict
 
 def color(ea, nbins, c):
     '''Color 'nbins' instructions starting from ea'''
-    colors = defaultdict(int)
-    colors.update({
-        'black' : 0x000000,
-        'red' : 0x0000FF,
-        'blue' : 0xFF0000,
-        'green' : 0x00FF00
-    })
+    colors = defaultdict(int, {
+            'black' : 0x000000,
+            'red' : 0x0000FF,
+            'blue' : 0xFF0000,
+            'green' : 0x00FF00
+        }
+    )
     for _ in range(nbins):
         idaapi.del_item_color(ea)
         idaapi.set_item_color(ea, colors[c])
