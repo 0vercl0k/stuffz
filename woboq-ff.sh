fa1921e75eef243e74c86c8bb9e3642db4a215b8
0vercl0k <0vercl0k@tuxfamily.org>
2019-04-07 09:53:19 -0700

    woboq it up.

---
+++ woboq-ff.sh
@@ -0,0 +1,7 @@
+# Axel '0vercl0k' Souchet - 28th March 2019
+OUT_DIR=ff-woboq
+DATA_DIR=/data
+rm -rf $OUT_DIR
+codebrowser_generator -a -o $OUT_DIR -b obj-x86_64-pc-linux-gnu -d $DATA_DIR -p Firefox:$(pwd) 
+codebrowser_indexgenerator -d $DATA_DIR $OUT_DIR
+cp -R ../woboq_codebrowser/data $OUT_DIR
